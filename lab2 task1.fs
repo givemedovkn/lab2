73d50bf04079240f36136ce73195e55ce421e78c
givemedovkn <vikadog6002@gmail.com>
2026-03-01 18:22:56 +0500

    Hellooo

---
+++ lab2 task1.fs
@@ -0,0 +1,32 @@
+﻿//11. На основе списка вещественных чисел получить список целых чисел, отбросив дробную часть.
+open System
+
+//Функция проверки натуральных чисел
+let checkNum num : int =
+    if num <= 0 
+    then 0
+    else 1
+
+//Функция запроса числа у пользователя
+let createFloatNum i : float =
+    printf "Введите вещественное число: "
+    let num =  float(Console.ReadLine())
+    num
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите количество чисел: "
+    let n = int(Console.ReadLine())
+    //проверка натурального числа
+    if (checkNum n) = 0 
+    then printfn "Некорректное число!"
+    else
+        //список вещественных чисел
+        let FloatNum = [for i in 1..n -> createFloatNum i]
+        //список целых чисел
+        let IntNum = List.map (fun floatNumber -> int floatNumber) FloatNum
+        printfn "Список вещественных чисел: "
+        printfn "%A " FloatNum
+        printfn "Список натуральных чисел: "
+        printfn "%A " IntNum
+    0
---
+++ lab2 task2.fs
@@ -0,0 +1,37 @@
+﻿//Список содержит строки. Сколько из них имеет чётную длину?
+open System
+
+//Функция проверки натуральных чисел
+let checkNum num : int =
+    if num <= 0 
+    then 0
+    else 1
+
+//Функция запроса строки
+let createList i : string =
+    printf "Введите строку: "
+    let str =  Console.ReadLine()
+    str
+
+[<EntryPoint>]
+let main _ =
+    printf "Введите количество строк: "
+    let n = int(Console.ReadLine())
+    //проверка натурального числа
+    if (checkNum n) = 0 
+    then printfn "Некорректное число!"
+    else
+        //список строк
+        let listLen = [for i in 1..n -> createList i]
+        //подсчет четных длин строк
+        let evenNum = 
+            listLen 
+            |> List.fold (fun summ str ->
+            if (str.Length)%2 = 0 
+            then summ+1 
+            else summ) 0
+        printfn "Список строк: "
+        printfn "%A " listLen
+        printfn "Из них строк с четной длиной: %i" evenNum
+
+    0
