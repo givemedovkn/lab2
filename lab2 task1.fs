8c4e3799b04acd4b164bd0a49f00d53e175d86c1
UsyninaVika <vikadog6002@gmail.com>
2026-03-02 12:08:51 +0500

    lab2 task1.fs

--- lab2 task1.fs
+++ lab2 task1.fs
@@ -1,11 +1,12 @@
-﻿//11. На основе списка вещественных чисел получить список целых чисел, отбросив дробную часть.
+//11. На основе списка вещественных чисел получить список целых чисел, отбросив дробную часть.
 open System
 
 //Функция проверки натуральных чисел
 let checkNum num : int =
-    if num <= 0 
-    then 0
-    else 1
+    if num <= 0 then 
+        0
+    else 
+        1
 
 //Функция запроса числа у пользователя
 let createFloatNum i : float =
@@ -17,14 +18,18 @@ let createFloatNum i : float =
 let main _ =
     printf "Введите количество чисел: "
     let n = int(Console.ReadLine())
-    //проверка натурального числа
-    if (checkNum n) = 0 
-    then printfn "Некорректное число!"
+    //Проверка натурального числа
+    if (checkNum n) = 0 then 
+        printfn "Некорректное число!"
     else
-        //список вещественных чисел
+        //Список вещественных чисел
         let FloatNum = [for i in 1..n -> createFloatNum i]
-        //список целых чисел
-        let IntNum = List.map (fun floatNumber -> int floatNumber) FloatNum
+        //Список целых чисел
+        let IntNum = 
+            List.map 
+                (fun floatNumber -> 
+                    int floatNumber) 
+                FloatNum
         printfn "Список вещественных чисел: "
         printfn "%A " FloatNum
         printfn "Список натуральных чисел: "
